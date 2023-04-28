2761da0ef662bdc9cdc2b5badbb60591ff9678cf
ZSuraj <surajjain.sj19@gmail.com>
2023-04-28 23:38:49 +0530

    activities page

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,7 +3,7 @@ import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
   return (
-    <div>
+    <div className='border border-black rounded apps'>
       <Navbar />
       <Component {...pageProps} />
     </div>
--- src/pages/activities.js
+++
@@ -1,7 +0,0 @@
-
-
-export default function Activities() {
-    return(
-        <div>Activities</div>
-    )
-}
