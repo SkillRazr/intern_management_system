9db0048ea2a872f273444e6d36ab8dc717a9edc4
tukuna30 <tukuna.patro@gmail.com>
2023-04-28 21:26:02 +0530

    add intern apis

--- src/components/InternDetails.js
+++ src/components/InternDetails.js
@@ -41,11 +41,11 @@ export default function InternDetails({ intern }) {
     <div className='interns-list-container flex flex-col items-center bg-black h-screen'>
       <div className='intern-container w-[98%] flex justify-between items-center border border-black rounded p-1.5 mt-1 mx-1 bg-white'>
         <div className="w-9 h-9 bg-gray-50 border border-black cursor-pointer rounded-full flex items-center justify-center" onClick={() => toUserPage(intern.id)}>
-          <span className="text-3xl mb-2">{intern.data.name.charAt(0)}</span>
+          <span className="text-3xl mb-2">{intern.name?.charAt(0)}</span>
         </div>
         <div className='intern-details-container w-9/12'>
-          <p className='ml-2 text-xl font-medium'>{intern.data.name.charAt(0).toUpperCase() + intern.data.name.slice(1)}</p>
-          <p className='ml-2 text-xs text-zinc-400'>{new Date(intern.data.joinedOn?.toDate()).toLocaleDateString(navigator.language, {day: 'numeric', month: 'short', year: 'numeric'})}</p>
+          <p className='ml-2 text-xl font-medium'>{intern.name?.charAt(0).toUpperCase() + intern.name?.slice(1)}</p>
+          <p className='ml-2 text-xs text-zinc-400'>{new Date(intern.joinDate).toLocaleDateString(navigator.language, {day: 'numeric', month: 'short', year: 'numeric'})}</p>
         </div>
         <div className='icon-container w-3/12 flex justify-evenly items-center'>
           <FontAwesomeIcon icon={faCircleCheck} className='cursor-pointer text-2xl text-green-600' onClick={() => updateAttendance("present")}/>
